21370e8dbefee0bf435161c234be74d9e786090b
echo <617928220@qq.com>
2018-05-21 08:36:40 +0800

    修改信息界面

--- app/src/main/java/edu/monash/swan/ass2/Movie/MovieInfo.java
+++ app/src/main/java/edu/monash/swan/ass2/Movie/MovieInfo.java
@@ -31,8 +31,8 @@ public class MovieInfo {
     public static MovieInfo getMoive(String name){
         String url = "http://v.juhe.cn/movie/index?key=fcfe38f356049760d2d33eef68668130&title="+name;
         MovieInfo movieInfo = null;
-       // String response = NetworkUtil.SendGet(url);
-        String response ="{\"resultcode\":\"200\",\"reason\":\"成功的返回\",\"result\":[{\"movieid\":\"10054\",\"rating\":\"9.1\",\"genres\":\"战争\\/喜剧\\/爱情\",\"runtime\":\"142 min\",\"language\":\"英语\",\"title\":\"阿甘正传\",\"poster\":\"http:\\/\\/img31.mtime.cn\\/mg\\/2014\\/06\\/17\\/145458.79721395_270X405X4.jpg\",\"writers\":\"温斯顿·格鲁姆,艾瑞克·罗斯\",\"film_locations\":\"美国\",\"directors\":\"罗伯特·泽米吉斯\",\"rating_count\":\"51749\",\"actors\":\"汤姆·汉克斯 Tom Hanks,罗宾·怀特 Robin Wright,加里·西尼斯 Gary Sinise,莎莉·菲尔德 Sally Field\",\"plot_simple\":\"阿甘的智商只有75，但凭借跑步的天赋，他顺利大学毕业。在越南战场，他结识了“捕虾迷”布巴和丹中尉。回国后，阿甘机缘巧合累积了大量资产。不过，钱并不是阿甘所看重的东西。阿甘和珍妮青梅竹马，可珍妮有自己的梦想……\",\"year\":\"1994\",\"country\":\"美国\",\"type\":null,\"release_date\":\"19940623\",\"also_known_as\":\"\"}],\"error_code\":0}";
+        String response = NetworkUtil.SendGet(url);
+       // String response ="{\"resultcode\":\"200\",\"reason\":\"成功的返回\",\"result\":[{\"movieid\":\"10054\",\"rating\":\"9.1\",\"genres\":\"战争\\/喜剧\\/爱情\",\"runtime\":\"142 min\",\"language\":\"英语\",\"title\":\"阿甘正传\",\"poster\":\"http:\\/\\/img31.mtime.cn\\/mg\\/2014\\/06\\/17\\/145458.79721395_270X405X4.jpg\",\"writers\":\"温斯顿·格鲁姆,艾瑞克·罗斯\",\"film_locations\":\"美国\",\"directors\":\"罗伯特·泽米吉斯\",\"rating_count\":\"51749\",\"actors\":\"汤姆·汉克斯 Tom Hanks,罗宾·怀特 Robin Wright,加里·西尼斯 Gary Sinise,莎莉·菲尔德 Sally Field\",\"plot_simple\":\"阿甘的智商只有75，但凭借跑步的天赋，他顺利大学毕业。在越南战场，他结识了“捕虾迷”布巴和丹中尉。回国后，阿甘机缘巧合累积了大量资产。不过，钱并不是阿甘所看重的东西。阿甘和珍妮青梅竹马，可珍妮有自己的梦想……\",\"year\":\"1994\",\"country\":\"美国\",\"type\":null,\"release_date\":\"19940623\",\"also_known_as\":\"\"}],\"error_code\":0}";
         JSONObject jsonObject;
         try {
             jsonObject = new JSONObject(response);
